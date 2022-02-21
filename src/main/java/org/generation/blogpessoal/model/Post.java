cece2f814095073cc6bb630d8d852218768540b6
Luciana Thomaz Soares <lucianat.s@hotmail.com>
2022-02-21 18:02:06 -0300

    @lucianaTSoares [Task: Update model Post]

--- src/main/java/org/generation/blogpessoal/model/Post.java
+++ src/main/java/org/generation/blogpessoal/model/Post.java
@@ -5,11 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_post")
@@ -30,35 +33,47 @@ public class Post {
     @Temporal(TemporalType.TIMESTAMP)
     private Date data = new java.sql.Date(System.currentTimeMillis());
 
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Tema tema;
+
     public int getId_post() {
         return id_post;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public String getTexto() {
-        return texto;
-    }
-
-    public Date getData() {
-        return data;
     }
 
     public void setId_post(int id_post) {
         this.id_post = id_post;
     }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
     public void setTitulo(String titulo) {
         this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
     }
 
     public void setTexto(String texto) {
         this.texto = texto;
     }
 
+    public Date getData() {
+        return data;
+    }
+
     public void setData(Date data) {
         this.data = data;
+    }
+
+    public Tema getTema() {
+        return tema;
+    }
+
+    public void setTema(Tema tema) {
+        this.tema = tema;
     }
 }
